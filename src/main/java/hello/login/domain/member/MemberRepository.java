7a86acbd686bea2ff5f5eb6b18ce4188365c8969
yupddok <yupddok@gmail.com>
2023-06-12 22:27:03 +0900

    로그인 처리1 - 쿠키,세션 > 로그인

--- src/main/java/hello/login/domain/member/MemberRepository.java
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -14,6 +14,7 @@ public class MemberRepository {
 
     public Member save(Member member) {
         member.setId(++sequence);
+        log.info("save: member={}", member);
         store.put(member.getId(), member);
         return member;
     }
